329b278b7b2cbc02dcc8e0136c57a6279d9e672b
lxxyeon <lxxyeon@gmail.com>
2021-07-25 18:52:07 +0900

    no message

--- Delegate Pattern/DelegatePattern_Swift/DelegatePattern_Swift/VC/XibViewController.swift
+++ Delegate Pattern/DelegatePattern_Swift/DelegatePattern_Swift/VC/XibViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class XibViewController: UIViewController {
     
     //앱에서 사용하기 위해 public 설정
-    public override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
