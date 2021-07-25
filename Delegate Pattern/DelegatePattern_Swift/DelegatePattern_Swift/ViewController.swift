c5946aa456927d6a271dd520fefcad579dfb05ff
lxxyeon <lxxyeon@gmail.com>
2021-07-25 16:16:14 +0900

    Delegate Pattern Swift ver.

---
+++ Delegate Pattern/DelegatePattern_Swift/DelegatePattern_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegatePattern_Swift
+//
+//  Created by leeyeon2 on 2021/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
