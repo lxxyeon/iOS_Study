ce52f3266ba9eccc60d889482a5100e2fbc801a2
lxxyeon <lxxyeon@gmail.com>
2021-07-31 17:28:16 +0900

    데이터 타입 확인 테스트

--- Delegate Pattern/DelegatePattern_Objc/DelegatePattern_Objc/VC/BViewController.m
+++ Delegate Pattern/DelegatePattern_Objc/DelegatePattern_Objc/VC/BViewController.m
@@ -21,6 +21,16 @@
 - (IBAction)dismissBVC:(id)sender {
     //3. 델리게이트 메소드 호출
     [self.delegate sendMessage: _messageField.text];
+    NSString *tmp_str = @"test";
+//    NSInteger tmp_int = 123
+    if ([tmp_str isKindOfClass:[NSString class]]) {
+//        NSLog(@"tmp type is %@ and value is %@", [tmp_str class], tmp_str);
+//    } else if ([tmp_str isKindOfClass:[NSInteger class]])  {
+//        NSLog(@"tmp type is %@ and value is %d", [tmp_str class], tmp_str);
+    }
+
+    NSLog(@"class type : %@", [tmp_str class]);
+    NSLog(@"class type : %@", [_messageField.text class]);
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
