5e1f4e1e95056c020f1cf38a4fe0f6b28bce5bf6
lxxyeon <lxxyeon@gmail.com>
2021-11-25 14:51:43 +0900

    indicatorView 추가

--- FunctionList_Swift/FunctionList_Swift/VC/ContainerViewController.swift
+++ FunctionList_Swift/FunctionList_Swift/VC/ContainerViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 
 class ContainerViewController: UIViewController, WKNavigationDelegate {
     
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
     @IBOutlet weak var testLabel: UILabel!
     @IBOutlet weak var webView: WKWebView!
     var testStr: String?
@@ -17,7 +18,8 @@ class ContainerViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.testLabel.text = testStr
-        
+        //이거 해줘야지 상속받은 메소드들 적용 가능
+        self.webView.navigationDelegate = self
         let url : URL! = URL(string: "https://www.google.com/")
         let request = URLRequest(url: url)
         self.webView.load(request)
@@ -32,6 +34,16 @@ class ContainerViewController: UIViewController, WKNavigationDelegate {
         let Avc = parent as! AViewController
         let myString = "Message From Container View"
         Avc.dataFromContainer(containerData: myString)
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicatorView.isHidden = false
+        indicatorView.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicatorView.stopAnimating()
+        indicatorView.isHidden = true
     }
 
 }
