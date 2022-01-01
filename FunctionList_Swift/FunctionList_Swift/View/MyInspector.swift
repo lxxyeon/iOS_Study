aaf7ab95b3276328092669012985b3ab29a080d5
lxxyeon <lxxyeon@gmail.com>
2022-01-01 19:47:42 +0900

    Inspector 추가

--- FunctionList_Swift/FunctionList_Swift/View/MyInspector.swift
+++ FunctionList_Swift/FunctionList_Swift/View/MyInspector.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class MyInspector: UIButton {
+    @IBInspectable var borderColor: UIColor{
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+        get {
+            let color = self.layer.borderColor ?? UIColor.clear.cgColor
+            return UIColor(cgColor: color)
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat{
+        set {
+            self.layer.borderWidth = newValue
+        }
+        get {
+            return self.layer.borderWidth
+        }
+    }
     
     @IBInspectable var cornerRadius: CGFloat{
         set {
-            layer.cornerRadius = newValue
+            self.layer.cornerRadius = newValue
         }
         get {
-            return layer.cornerRadius
-            
+            return self.layer.cornerRadius
         }
     }
 }
