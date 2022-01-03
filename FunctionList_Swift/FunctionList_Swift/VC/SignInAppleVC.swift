2f2d3679ce7930bc18904c696153a0f99dbd59ce
lxxyeon <lxxyeon@gmail.com>
2022-01-03 19:31:33 +0900

    social login 추가

--- FunctionList_Swift/FunctionList_Swift/VC/SignInAppleVC.swift
+++ FunctionList_Swift/FunctionList_Swift/VC/SignInAppleVC.swift
@@ -10,14 +10,12 @@ import AuthenticationServices
 
 class SignInAppleVC: UIViewController  {
     @IBOutlet weak var signInBtn: UIButton!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         signInBtn.setBackgroundColor(.black, for: .selected)
     }
     
-
     //애플 소셜 로그인
     @IBAction func appleLogin(_ sender: Any) {
         let appleIDProvider = ASAuthorizationAppleIDProvider()
@@ -78,5 +76,47 @@ extension UIButton {
         UIGraphicsEndImageContext()
          
         self.setBackgroundImage(backgroundImage, for: state)
+    }
+}
+
+@IBDesignable
+class DesignableUITextField: UITextField {
+    
+    // Provides left padding for images
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.leftViewRect(forBounds: bounds)
+        textRect.origin.x += leftPadding
+        return textRect
+    }
+    
+    @IBInspectable var leftImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var leftPadding: CGFloat = 0
+    
+    @IBInspectable var color: UIColor = UIColor.lightGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        if let image = leftImage {
+            leftViewMode = UITextField.ViewMode.always
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+            imageView.tintColor = color
+            leftView = imageView
+        } else {
+            leftViewMode = UITextField.ViewMode.never
+            leftView = nil
+        }
+        
+        // Placeholder text color
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: color])
     }
 }
