2155abf3c17e532b73923f43d0a91f8ea48f1e60
lxxyeon <lxxyeon@gmail.com>
2022-01-10 19:55:14 +0900

    model, VC 분리

--- FunctionList_Swift/FunctionList_Swift/VC/HttpNetworkManagerViewController.swift
+++ FunctionList_Swift/FunctionList_Swift/VC/HttpNetworkManagerViewController.swift
@@ -10,5 +10,73 @@ import Alamofire
 
 // Alamofire 사용해서 Network 통신하기
 class HttpNetworkManagerViewController: UIViewController {
+    
+    private var handler: ((Result<[UserData], Error>) -> Void)!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        handler = { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let userDatas):
+                guard let userData = userDatas.first else { return }
+                self.setInfo(by: userData)
+            case .failure(let error):
+                print("Error", error.localizedDescription)
+                self.setError()
+            }
+        }
+    }
+    
+    private func setInfo(by data: UserData) {
+        resultLabel.text = """
+                               ID: \(data.id)\n
+                               Title: \(data.title)\n
+                               UserId: \(data.userId)\n
+                               Body: \(data.body)\n
+                              """
+    }
+    
+    private func setError() {
+        resultLabel.text = """
+                               ID: Error\n
+                               Title: Error\n
+                               UserId: Error\n
+                               Body: Error\n
+                              """
+        
+    }
+}
 
+extension HttpNetworkManagerViewController {
+    @IBAction private func created(_ sender: UIButton) {
+        guard let url = URL(string: "https://github.com/kor45cw/") else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction private func GET1(_ sender: UIButton) {
+        API.shared.get1(completionHandler: handler)
+    }
+    
+    @IBAction private func GET2(_ sender: UIButton) {
+        API.shared.get2(completionHandler: handler)
+    }
+    
+    @IBAction private func POST(_ sender: UIButton) {
+        API.shared.post(completionHandler: handler)
+    }
+    
+    @IBAction private func PUT(_ sender: UIButton) {
+        API.shared.put(completionHandler: handler)
+    }
+    
+    @IBAction private func PATCH(_ sender: UIButton) {
+        API.shared.patch(completionHandler: handler)
+    }
+    
+    @IBAction private func DELETE(_ sender: UIButton) {
+        API.shared.delete(completionHandler: handler)
+    }
 }
